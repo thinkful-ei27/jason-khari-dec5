cc6efa287779053b2d7526424cc989d76414855d
publikwerker <hello@publikwerks.com>
2018-12-05 12:38:44 -0800

    added arrays in objects and arrays of objects

--- gistObject.js
+++ gistObject.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// 1. Object initializers and methods
 let loaf = {
   flour: 300,
   water: 210,
@@ -10,6 +12,7 @@ let loaf = {
 //console.log(loaf.flour);
 //console.log(loaf.hydration());
 
+// 2. Iterating over an object's properties
 let mumbler = {
   foo: 'foo value',
   bar: 'bar value',
@@ -17,6 +20,27 @@ let mumbler = {
   quux: 'quux value',
   spam: 'spam value'
 };
-for(const key in mumbler){
-  console.log(`${key}: ${mumbler[key]}`);
-}
+//for(const key in mumbler){
+// console.log(`${key}: ${mumbler[key]}`);
+//}
+
+// 3. Arrays in objects
+const fullMealList = {
+  meals: [
+    'breakfast', 'second breakfast', 
+    'elevenses', 'lunch', 'afternoon tea',
+    'dinner', 'supper'
+  ]
+};
+console.log(`A Hobbit's fourth meal is ${fullMealList.meals[3]}.`);
+
+// 4. Arrays of Objects
+let theArray = [
+  {name: 'Harry', jobTitle: 'Auror'},
+  {name: 'Hermione', jobTitle: 'Minister of Magic'},
+  {name: 'Ron', jobTitle: 'Stay at home father'}
+];
+
+theArray.forEach(function(object) {
+  console.log(`Name: ${object.name}, Job Title: ${object.jobTitle}`);
+});
